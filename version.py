d0dc78573584e1c67ee60d1027e20030444eb8f2
ddella <48737096+ddella@users.noreply.github.com>
2023-06-17 11:19:52 -0400

    Initial commit

---
+++ version.py
@@ -0,0 +1,17 @@
+import sys
+import platform
+
+"""
+Get the Python's version
+"""
+
+print("Hello from Jenkins 😀\n")
+# check the Python version using the sys module
+print(f'sys module: {sys.version}')
+
+# check the Python version using the platform module
+print(f'platform module: {platform.python_version()}')
+
+# version number in tuple format
+print(f'tuple format: {sys.version_info}')
+print(platform.python_version_tuple())
